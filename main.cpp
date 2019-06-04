a247ffca1290d0651f23a2a23280e347d7e79dd8
Tomoloju Victor <tomoloju45@gmail.com>
2019-06-04 10:32:06 -1100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ for(uint64_t i = 0 ; i<strlen(ptr); ++i){
                 ++ch;
         }  ///
 
-
+        break;
     }
 
 }
@@ -40,18 +40,56 @@ ptr[num_space] = '\0';
 
 }
 
+void trim(string *ptr){
+
+uint64_t num_space = 0;
+
+for(uint64_t i = 0 ; i<ptr->length()+1; ++i){
+
+    if((*ptr)[i] == ' '){
+            ++num_space;
+
+            uint64_t te = i;
+            uint64_t ch = i;
+                    //ptr->length()+1
+
+        while(ch<ptr->length()+1){
+
+                if((*ptr)[ch+1] != ' '){
+
+            (*ptr)[te] = (*ptr)[ch+1];
+                ++te;
+                }
+
+                    else{
+                        ++num_space;
+                    }
+                ++ch;
+        }  ///
+
+            break;
+    }
+
+}
+
+for(int i = (ptr->length() - num_space); i<ptr->length(); ++i){
+    (*ptr)[i] = NULL;
+}
+
+}
 
 int main()
 {
 
-char vic[] = "c++ is the best";
-/*
-for(auto i : vic){
-    cout<<i<<endl;
-}
-*/
+/// c string Test.....
+char test[] = "c++ is the            best";
 
-trim(vic);
-cout<<vic<<endl;
+trim(test);
+cout<<test<<endl;
 
+///string Test...........
+string test2 = "c++ is the            best";
+trim(&test2);
+cout<<test2<<endl;
+    
 }
